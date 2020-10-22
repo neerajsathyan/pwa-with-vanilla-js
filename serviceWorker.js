1388c6dbd9734ffdcd626134ede12b10b31a8b0d
Neeraj <neeraj.sathian@gmail.com>
2020-10-22 21:54:45 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,18 @@
 const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
+  "/pwa-with-vanilla-js",
+  "/pwa-with-vanilla-js/index.html",
+  "/pwa-with-vanilla-js/css/style.css",
+  "/pwa-with-vanilla-js/js/app.js",
+  "/pwa-with-vanilla-js/images/coffee1.jpg",
+  "/pwa-with-vanilla-js/images/coffee2.jpg",
+  "/pwa-with-vanilla-js/images/coffee3.jpg",
+  "/pwa-with-vanilla-js/images/coffee4.jpg",
+  "/pwa-with-vanilla-js/images/coffee5.jpg",
+  "/pwa-with-vanilla-js/images/coffee6.jpg",
+  "/pwa-with-vanilla-js/images/coffee7.jpg",
+  "/pwa-with-vanilla-js/images/coffee8.jpg",
+  "/pwa-with-vanilla-js/images/coffee9.jpg"
 ];
 
 self.addEventListener("install", installEvent => {
