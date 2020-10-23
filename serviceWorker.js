7572aa1dd43fa5113f2c8f32104b506dfc2a2025
Neeraj <neeraj.sathian@gmail.com>
2020-10-23 02:14:15 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,19 @@
-'use strict';
-
-const cacheVersion = '"dev-coffee-site-v1"';
+const staticDevCoffee = "dev-coffee-site-v1";
+/*const assets = [
+  "/pwa-with-vanilla-js",
+  "/pwa-with-vanilla-js/index.html",
+  "/pwa-with-vanilla-js/css/style.css",
+  "/pwa-with-vanilla-js/js/app.js",
+  "/pwa-with-vanilla-js/images/coffee1.jpg",
+  "/pwa-with-vanilla-js/images/coffee2.jpg",
+  "/pwa-with-vanilla-js/images/coffee3.jpg",
+  "/pwa-with-vanilla-js/images/coffee4.jpg",
+  "/pwa-with-vanilla-js/images/coffee5.jpg",
+  "/pwa-with-vanilla-js/images/coffee6.jpg",
+  "/pwa-with-vanilla-js/images/coffee7.jpg",
+  "/pwa-with-vanilla-js/images/coffee8.jpg",
+  "/pwa-with-vanilla-js/images/coffee9.jpg"
+];*/
 
 this.addEventListener('activate', function (event) {
     event.waitUntil(
@@ -8,7 +21,7 @@ this.addEventListener('activate', function (event) {
         caches.keys().then(function (keyList) {
             return Promise.all(
                 keyList.filter(function (key) {
-                    return key != cacheVersion;
+                    return key != staticDevCoffee;
                 }).map(function (key) {
                     return caches.delete(key);
                 }));
@@ -16,23 +29,21 @@ this.addEventListener('activate', function (event) {
     );
 });
 
-this.addEventListener('fetch', function (event) {
-    let originalResponse;
-
-    event.respondWith(
-        fetch(event.request.clone())
-            .then(function (response) {
-                // Check that the status is OK
-                if (/^0|([123]\d\d)|(40[14567])|410$/.test(response.status)) {
-
-                    return response.clone();
-                }
-
-                originalResponse = response;
-                throw new Error('Bad response');
-            })
-            .catch(function (error) {
-                throw new Error('No Network!');
-            })
-    );
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request).then(resp => {
+      	caches.open(staticDevCoffee).then(cache => {
+           cache.put(fetchEvent.request, resp);
+        });
+	 return resp;
+      });
+    }).catch(err => {
+	 //No cache no network 503 service unaivalable..
+            var myBlob = new Blob();
+            var init = { "status" : 503 , "statusText" : "Service Unaivailable!" };
+            var myResponse = new Response(myBlob,init);
+            return myResponse
+    })
+  );
 });
